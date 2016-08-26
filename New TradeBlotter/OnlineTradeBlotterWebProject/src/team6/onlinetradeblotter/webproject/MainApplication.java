db5eab339e0514935be14d57d9303cbd81019327
sushil013 <sushil.singh013@gmail.com>
2016-08-26 14:40:19 +0530

    Revert "Revert "Updates to Trade Blotter""
    This reverts commit 666e31e1e4bcfeb91104e7943d2604cc971daffc.

---
+++ New TradeBlotter/OnlineTradeBlotterWebProject/src/team6/onlinetradeblotter/webproject/MainApplication.java
@@ -0,0 +1,22 @@
+package team6.onlinetradeblotter.webproject;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class MainApplication extends Application {
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> empty = new HashSet<Class<?>>();
+	
+	public MainApplication(){
+		singletons.add(new BlotterResource());
+		}
+	
+	@Override
+	public Set<Class<?>> getClasses() { return empty; }
+	@Override
+	public Set<Object> getSingletons() { return singletons; }
+}
