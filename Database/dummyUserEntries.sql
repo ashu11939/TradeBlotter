c9e5ec363cfcb281f6c4e8e1139e326ab0babffa
Bhavya Tak <takbhavya678@gmail.com>
2016-08-26 11:45:20 +0530

    dummy entries for user table

---
+++ Database/dummyUserEntries.sql
@@ -0,0 +1,7 @@
+INSERT into tradeblotter.users (FirstName,LastName,Username,Password,LogoutTime) values ('Bhavya','Tak','btak','bt123','2012-06-18 10:34:09' );
+INSERT into tradeblotter.users (FirstName,LastName,Username,Password,LogoutTime) values ('Vijay','Meena','vmeena','vm123','2012-06-18 10:36:09');
+INSERT into tradeblotter.users (FirstName,LastName,Username,Password,LogoutTime) values ('Sushil','Singh','ssingh','ss123','2012-06-18 22:38:09');
+INSERT into tradeblotter.users (FirstName,LastName,Username,Password,LogoutTime) values ('Nitish','Kumar','nkumar','nk123','2012-08-11 22:48:09');
+INSERT into tradeblotter.users (FirstName,LastName,Username,Password,LogoutTime) values ('Vineet','V','vv','vv123','2012-06-18 7:38:09');
+INSERT into tradeblotter.users (FirstName,LastName,Username,Password,LogoutTime) values ('Ashutosh','Kumar','akumar','ak123','2016-06-18 22:38:09');
+INSERT into tradeblotter.users (FirstName,LastName,Username,Password,LogoutTime) values ('Rucha','H','rh','rh123','2015-10-18 22:38:09');
