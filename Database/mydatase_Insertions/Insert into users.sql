99039f23f4b1106caba736f3deec7e160f55cfcf
Nitish Kumar <nitishkumar8663@gmail.com>
2016-08-27 12:08:28 +0530

    Insert statements for users table

---
+++ Database/mydatase_Insertions/Insert into users.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM mydatabase.users
+-- Date: 2016-08-27 12:06
+*/
+use mydatabase;
+INSERT INTO `users` (`UserID`,`FirstName`,`LastName`,`Username`,`Password`,`LogoutTime`) VALUES (1,'Bhavya','Tak','btak','bt123','2012-06-18 10:34:09');
+INSERT INTO `users` (`UserID`,`FirstName`,`LastName`,`Username`,`Password`,`LogoutTime`) VALUES (2,'Vijay','Meena','vmeena','vm123','2012-06-18 10:36:09');
+INSERT INTO `users` (`UserID`,`FirstName`,`LastName`,`Username`,`Password`,`LogoutTime`) VALUES (3,'Sushil','Singh','ssingh','ss123','2012-06-18 22:38:09');
+INSERT INTO `users` (`UserID`,`FirstName`,`LastName`,`Username`,`Password`,`LogoutTime`) VALUES (4,'Nitish','Kumar','nkumar','nk123','2012-08-11 22:48:09');
+INSERT INTO `users` (`UserID`,`FirstName`,`LastName`,`Username`,`Password`,`LogoutTime`) VALUES (5,'Vineet','V','vv','vv123','2012-06-18 07:38:09');
+INSERT INTO `users` (`UserID`,`FirstName`,`LastName`,`Username`,`Password`,`LogoutTime`) VALUES (6,'Ashutosh','Kumar','akumar','ak123','2016-06-18 22:38:09');
+INSERT INTO `users` (`UserID`,`FirstName`,`LastName`,`Username`,`Password`,`LogoutTime`) VALUES (7,'Rucha','H','rh','rh123','2015-10-18 22:38:09');
